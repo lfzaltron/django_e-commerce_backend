2f6f145bc74465e567343667bcc9b186d9af2630
Luis Fernando Zaltron <lfzaltron@gmail.com>
2019-08-14 22:05:36 -0300

    polls

--- atelier/settings.py
+++ atelier/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'atelier.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zaltron',
+        'USER': 'zaltron',
+        'PASSWORD': '450Pass@atelier',
+        'HOST': 'pgsql.zaltron.com.br',
     }
 }
 
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
