452c851ebc05a1a5b965c9d102a3dd288503a99b
Luis Fernando Zaltron <luis.zaltron@procempa.com.br>
2019-08-23 16:21:32 -0300

    adicionando tipos Pedido e ItemPedido

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Pedido, ItemPedido
 
 # Register your models here.
+class ItemPodidoInline(admin.StackedInline):
+    model = ItemPedido
+
+
+class PedidosAdmin(admin.ModelAdmin):
+    inlines = [ItemPodidoInline]
+    list_display = ('data', 'quantidade_itens')
+
 admin.site.register(Produto)
+admin.site.register(Pedido, PedidosAdmin)
+#admin.site.register(ItemPedido)
--- pedidos/models.py
+++ pedidos/models.py
@@ -6,6 +6,40 @@ class Produto(models.Model):
     descricao = models.TextField(max_length=400, name='Descrição')
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     foto = models.ImageField(upload_to='produtos/', default='pic_folder/None/no-img.jpg')
+    #categoria?
 
     def __str__(self):
         return self.nome
+
+class Pedido(models.Model):
+    # utilizado já para carrinho de compras? acho que sim
+    STATUS_PEDIDO = (
+        (0, 'Carrinho'),
+        (1, 'Aguardando confirmação do restaurante'),
+        (2, 'Confirmado'),
+        (3, 'Em produção'),
+        (4, 'Aguardando retirada'),
+        (5, 'Finalizado'),
+    ) # https://stackoverflow.com/questions/12725720/django-choices-how-to-set-default-option
+    status = models.IntegerField(null=False, choices=STATUS_PEDIDO, default=0)
+    # cliente
+    # forma de pagamento
+    data = models.DateTimeField() # DataHora da CONFIRMAÇÃO do pedido.
+
+    def __str__(self):
+        return str(self.data)
+
+    def quantidade_itens(self):
+        itens = ItemPedido.objects.filter(pedido=self)
+        return sum(map(lambda x: x.quantidade, itens))
+
+class ItemPedido(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+    #adicional
+    #remover?
+    observacao = models.CharField(max_length=200, blank=True)
+
+
+#Não esquecer dos testes
