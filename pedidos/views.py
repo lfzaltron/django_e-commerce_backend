4675a35ada834f2d077db03381732402919d10bd
Luis Fernando Zaltron <luis.zaltron@procempa.com.br>
2019-08-27 14:21:05 -0300

    login obrigatorio

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+#https://docs.djangoproject.com/en/2.1/topics/auth/default/
 
 from .models import Produto
 # Create your views here.
 
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin, generic.ListView):
+    login_url = '/admin/login/'
+
     model = Produto
     template_name = 'pedidos/index.html'
     context_object_name = 'produtos_list'
